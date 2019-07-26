88b3aeea21247727e981b3f8391acc4ab3fb3970
Sammy <1014268@student.sae.edu.au>
2019-07-26 19:34:38 +0800

    Update_26.07.19
    Added gate triggers and targets
    Added teleport script
    Added skybox

---
+++ Assets/Scripts/Gates.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gates : MonoBehaviour
+{
+    public Transform gateTarget;
+    public GameObject playerCharacter;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        playerCharacter.transform.position = gateTarget.transform.position;
+        Debug.Log(gateTarget.transform.position);
+    }
+}    
