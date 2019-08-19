9b9308956e63611a09b60e948c248cf7cc92acbf
Sammy <1014268@student.sae.edu.au>
2019-08-19 15:14:04 +0800

    Added Visualisers

---
+++ Assets/Scripts/BandSampling.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BandSampling : MonoBehaviour
+{
+    AudioSampling audioSampling;
+
+    [Header("Sampling Options")]
+    public int _band;
+    public float _minScale, _maxScale;
+    public bool _use8Samples;
+    public bool _use64Samples;
+    public bool _useFrequency;
+    public bool _useAmplitude;
+    public bool _useBuffer;
+
+    //Setting other values
+    Material _material;
+    Color _colour;
+
+    private float _audioBand;
+    private float _audioBandBuffer;
+    private float _amplitude;
+    private float _amplitudeBuffer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSampling = GameObject.FindObjectOfType<AudioSampling>();
+        _material = GetComponent<MeshRenderer>().materials[0];
+        _colour = _material.color;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckSampleSize();
+        FreqScale();
+        AmpScale();
+    }
+
+    void CheckSampleSize()
+    {
+        if (_use8Samples)
+        {
+            _use64Samples = false;
+            _audioBand = audioSampling._audioBand8[_band];
+            _audioBandBuffer = audioSampling._audioBandBuffer8[_band];
+            _amplitude = audioSampling._Amplitude;
+            _amplitudeBuffer = audioSampling._AmplitudeBuffer;
+        }
+        else if (_use64Samples)
+        {
+            _use8Samples = false;
+            _audioBand = audioSampling._audioBand64[_band];
+            _audioBandBuffer = audioSampling._audioBandBuffer64[_band];
+            _amplitude = audioSampling._Amplitude;
+            _amplitudeBuffer = audioSampling._AmplitudeBuffer;
+        }
+        else
+        {
+            Debug.Log("Check Sample Size!");
+        }
+    }
+
+    void FreqScale()
+    {
+        if (_useFrequency)
+        {
+            if (_useBuffer) //To use buffering
+            {
+                transform.localScale = new Vector3(transform.localScale.x, ((_audioBandBuffer * _maxScale) + _minScale), transform.localScale.z);
+                Color _colour = new Color(_audioBandBuffer, _audioBandBuffer, _audioBandBuffer);
+                _material.SetColor("_EmissionColor", _colour);
+            }
+            else if (!_useBuffer) //To use strict scaling
+            {
+                transform.localScale = new Vector3(transform.localScale.x, ((_audioBand * _maxScale) + _minScale), transform.localScale.z);
+                Color _colour = new Color(_audioBand, _audioBand, _audioBand);
+                _material.SetColor("_EmissionColor", _colour);
+            }
+        }
+        else if (!_useFrequency)
+        {
+
+        }
+    }
+
+    void AmpScale()
+    {
+        if (_useAmplitude)
+        {
+            if (_useBuffer) //To use buffering
+            {
+                transform.localScale = new Vector3(transform.localScale.x, ((_amplitudeBuffer * _maxScale) + _minScale), transform.localScale.z);
+                Color _colour = new Color(_amplitudeBuffer, _amplitudeBuffer, _amplitudeBuffer);
+                _material.SetColor("_EmissionColor", _colour);
+            }
+            else if (!_useBuffer) //To use strict scaling
+            {
+                transform.localScale = new Vector3(transform.localScale.x, ((_amplitude * _maxScale) + _minScale), transform.localScale.z);
+                Color _colour = new Color(_amplitude, _amplitude, _amplitude);
+                _material.SetColor("_EmissionColor", _colour);
+            }
+        }
+        else if (!_useAmplitude)
+        {
+
+        }
+
+
+    }
+}
---
+++ Assets/Visualiser_BasicExport/AudioSampling_Basic.cs
@@ -0,0 +1,224 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioSampling_Basic : MonoBehaviour
+{
+    AudioSource _audioSource;
+
+    //Samples Frequencies
+    private float[] _samples = new float[512];
+
+    //Audio_8
+    private float[] _freqBand8 = new float[8];
+    private float[] _bandBuffer8 = new float[8];
+    private float[] _bufferDecrease8 = new float[8];
+    private float[] _freqBandHighest8 = new float[8];
+    public float[] _audioBand8, _audioBandBuffer8;
+
+    //Audio_64
+    private float[] _freqBand64 = new float[64];
+    private float[] _bandBuffer64 = new float[64];
+    private float[] _bufferDecrease64 = new float[64];
+    private float[] _freqBandHighest64 = new float[64];
+    public float[] _audioBand64, _audioBandBuffer64;
+
+
+    [HideInInspector]
+    private float _AmplitudeHighest;
+    public float _Amplitude, _AmplitudeBuffer;
+    public float _bufferSpeed;
+
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+
+        //Set Buffer Speed
+        _bufferSpeed = 1.2f;
+
+        Set8();
+        Set64();
+    }
+
+    void Update()
+    {
+        GetSpectrum();
+        GetAmplitude();
+        Sample8();
+        Sample64();
+    }
+
+    //Gets ALL Spectrum Values
+    void GetSpectrum()
+    {
+        _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+    }
+
+    //Gets ALL Amplitude Values
+    void GetAmplitude()
+    {
+        float _CurrentAmplitude = 0;
+        float _CurrentAmplitudeBuffer = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            _CurrentAmplitude += _audioBand8[i];
+            _CurrentAmplitudeBuffer += _audioBandBuffer8[i];
+        }
+        if (_CurrentAmplitude > _AmplitudeHighest)
+        {
+            _AmplitudeHighest = _CurrentAmplitude;
+        }
+        else
+        {
+            _AmplitudeHighest = 1;
+        }
+
+        _Amplitude = _CurrentAmplitude / _AmplitudeHighest;
+        _AmplitudeBuffer = _CurrentAmplitudeBuffer / _AmplitudeHighest;
+    }
+
+    //For 8 Samples
+    void Set8()
+    {
+        _audioBand8 = new float[8];
+        _audioBandBuffer8 = new float[8];
+    } //Call at Start
+
+    void Sample8()
+    {
+        CreateFreqBands8();
+        BandBuffer8();
+        CreateAudioBands8();
+    }  //Call at Update
+
+    void CreateFreqBands8()
+    {
+        int count = 0;
+
+        for (int i = 0; i < 8; i++)
+        {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += _samples[count] * (count + 1);
+                count++;
+            }
+            average /= count;
+            _freqBand8[i] = average * 10;
+        }
+    } //Held in Sample8
+
+    void BandBuffer8()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            if (_freqBand8[i] > _bandBuffer8[i])
+            {
+                _bandBuffer8[i] = _freqBand8[i];
+                _bufferDecrease8[i] = 0.005f;
+            }
+            if (_freqBand8[i] < _bandBuffer8[i])
+            {
+                _bandBuffer8[i] -= _bufferDecrease8[i];
+                _bufferDecrease8[i] *= _bufferSpeed;
+            }
+        }
+    } //Held in Sample8
+
+    void CreateAudioBands8()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            if (_freqBand8[i] > _freqBandHighest8[i])
+            {
+                _freqBandHighest8[i] = _freqBand8[i];
+            }
+            else
+            {
+                _freqBandHighest8[i] = 1f;
+            }
+            _audioBand8[i] = (_freqBand8[i] / _freqBandHighest8[i]);
+            _audioBandBuffer8[i] = (_bandBuffer8[i] / _freqBandHighest8[i]);
+        }
+    } //Held in Sample8
+
+    //For 64 Samples
+    void Set64()
+    {
+        _audioBand64 = new float[64];
+        _audioBandBuffer64 = new float[64];
+    } //Call at Start
+
+    void Sample64()
+    {
+        CreateFreqBands64();
+        BandBuffer64();
+        CreateAudioBands64();
+    }  //Call at Update
+
+    void CreateFreqBands64()
+    {
+        int count = 0;
+        int sampleCount = 1;
+        int power = 0;
+
+        for (int i = 0; i < 64; i++)
+        {
+            float average = 0;
+            if (i == 16 || i == 32 || i == 40 || i == 48 || i == 56)
+            {
+                power++;
+                sampleCount = (int)Mathf.Pow(2, 1) * 2;
+                if (power == 3)
+                {
+                    sampleCount -= 2;
+                }
+            }
+
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += _samples[count] * (count + 1);
+                count++;
+            }
+            average /= count;
+            _freqBand64[i] = average * 10;
+        }
+    } //Held in Sample64
+
+    void BandBuffer64()
+    {
+        for (int i = 0; i < 64; i++)
+        {
+            if (_freqBand64[i] > _bandBuffer64[i])
+            {
+                _bandBuffer64[i] = _freqBand64[i];
+                _bufferDecrease64[i] = 0.005f;
+            }
+            if (_freqBand64[i] < _bandBuffer64[i])
+            {
+                _bandBuffer64[i] -= _bufferDecrease64[i];
+                _bufferDecrease64[i] *= _bufferSpeed;
+            }
+        }
+    } //Held in Sample64
+
+    void CreateAudioBands64()
+    {
+        for (int i = 0; i < 64; i++)
+        {
+            if (_freqBand64[i] > _freqBandHighest64[i])
+            {
+                _freqBandHighest64[i] = _freqBand64[i];
+            }
+            else
+            {
+                _freqBandHighest64[i] = 1f;
+            }
+            _audioBand64[i] = (_freqBand64[i] / _freqBandHighest64[i]);
+            _audioBandBuffer64[i] = (_bandBuffer64[i] / _freqBandHighest64[i]);
+        }
+    } //Held in Sample64
+}
