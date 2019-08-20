dcd83f212972a7179ef56a81df5fd7b571257350
Sammy Stanford <1014268@student.sae.edu.au>
2019-08-20 08:16:46 +0800

    Main Menu and Asset Showcase

---
+++ Assets/Scripts/MouseOver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOver : MonoBehaviour
+{
+    public bool _playGame;
+    public bool _assetShowcase;
+
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+}
